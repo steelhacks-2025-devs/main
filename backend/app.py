78c27f17f3d9fd53063a3ba2bea57057c9208af9
shane-thoma <masterfulelf@gmail.com>
2025-09-20 19:54:52 -0400

    Moved ml.ipynb to clustering.ipynb and made pca.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 import pandas as pd
+from prox import count_amenities_by_distance
+from pca import pca_scoring
 
 # init flask
 app = Flask(__name__, template_folder='../frontend', static_folder='../frontend/static')
@@ -26,6 +28,21 @@ def find():
 
 @app.route('/results')
 def results():
+    # Get csv file path
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    property_csv = os.path.join(BASE_DIR, 'datasets', 'property-assessments-combined.csv')
+
+    # Read PGH property data
+    df = pd.read_csv(property_csv)
+
+    # Filter down to only residential properties
+    df = df[df['CLASS'] == 'R']
+    
+    # Example count_amenities_by_distance call
+    test_row = df[0]
+    for amenity in ['hospital', 'transportation']:
+        print(count_amenities_by_distance(f'{test_row['PROPERTYHOUSENUM']} {test_row['PROPERTYADDRESS']}, Pittsburgh, PA {test_row['PROPERTYZIP']}', amenity, [1, 3, 5]))
+
     return render_template('results.html')
 
 # Run the Flask server
