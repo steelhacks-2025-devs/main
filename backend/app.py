2a516e709e3af7a7aee96663845e4e66a003968c
shane-thoma <masterfulelf@gmail.com>
2025-09-20 21:45:25 -0400

    Set up results page to use selected neighborhood from index page

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 import pandas as pd
-from prox import count_amenities_by_distance
+# from prox import count_amenities_by_distance
 from pca import pca_scoring
 
 # init flask
@@ -24,27 +24,41 @@ def home():
 
 @app.route('/find')
 def find():
+    # Get csv file path
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    property_csv = os.path.join(BASE_DIR, 'datasets', 'finalpt5.csv')
+
+    # Read PGH property into a DataFrame
+    df = pd.read_csv(property_csv)
+
+    neighborhood = request.form.get('neighborhood-list')
+    print(pca_scoring(df[df['NEIGHBORHOOD'] == neighborhood]))
     
     return render_template('find.html')
 
-@app.route('/results')
+@app.route('/results', methods=['POST'])
 def results():
+    print(request.form)
+    # Get neighborhood selected by user on index page
+    selected_neighborhood = request.form.get('neighborhood-list')
+    print(selected_neighborhood)
+
     # Get csv file path
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    property_csv = os.path.join(BASE_DIR, 'datasets', 'property-assessments-combined.csv')
+    property_csv = os.path.join(BASE_DIR, 'datasets', 'finalpt5.csv')
 
-    # Read PGH property data
+    # Read PGH property into a DataFrame
     df = pd.read_csv(property_csv)
 
-    # Filter down to only residential properties
-    df = df[df['CLASS'] == 'R']
+    print(df['NEIGHDESC'])
+    print(df[df['NEIGHDESC'] == selected_neighborhood.upper()].head())
     
-    # Example count_amenities_by_distance call
-    test_row = df[0]
-    for amenity in ['hospital', 'transportation']:
-        print(count_amenities_by_distance(f'{test_row['PROPERTYHOUSENUM']} {test_row['PROPERTYADDRESS']}, Pittsburgh, PA {test_row['PROPERTYZIP']}', amenity, [1, 3, 5]))
+    # # Example count_amenities_by_distance call
+    # test_row = df[0]
+    # for amenity in ['hospital', 'transportation']:
+    #     print(count_amenities_by_distance(f'{test_row['PROPERTYHOUSENUM']} {test_row['PROPERTYADDRESS']}, Pittsburgh, PA {test_row['PROPERTYZIP']}', amenity, [1, 3, 5]))
 
-    return render_template('results.html')
+    return render_template('results.html', neighborhood=selected_neighborhood)
 
 # Run the Flask server
 if __name__ == '__main__':
