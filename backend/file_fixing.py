5fb0daaa18b515640ae8783d855ebb3f0577c538
Joseph Maggio <56551598+joem23@users.noreply.github.com>
2025-09-20 20:04:57 -0400

    add further cleaned data

--- backend/file_fixing.py
+++ backend/file_fixing.py
@@ -5,7 +5,7 @@ import os
 # Get csv file paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # csv_files = [os.path.join(BASE_DIR, 'datasets', f'property-assessments-{i}.csv') for i in range(1, 5)]
-csv_file = os.path.join(BASE_DIR, 'datasets', 'finalpt1.csv')
+csv_file = os.path.join(BASE_DIR, 'datasets', 'finalpt3.csv')
 # Load the CSV file into a pandas DataFrame
 print(f"Loading CSV file: {csv_file}")
 global df
@@ -14,10 +14,10 @@ df = pd.read_csv(csv_file)
 print(f"Original shape: {df.shape}")
 
 def fix():
-    df.drop('PROPERTYHOUSENUM', axis=1, inplace=True)
+    df.drop(columns=['CLASS', 'CLASSDESC', 'SALEPRICE', 'COUNTYTOTAL', 'LOCALTOTAL', 'GRADE', 'GRADEDESC'], inplace=True)
 
 fix()
 
 print(f"New shape: {df.shape}")
 
-df.to_csv(os.path.join(BASE_DIR, 'datasets', 'finalpt2.csv'), index=False)
+df.to_csv(os.path.join(BASE_DIR, 'datasets', 'finalpt4.csv'), index=False)
